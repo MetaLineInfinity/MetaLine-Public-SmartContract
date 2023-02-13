fd8ec0473eabb2f2e907f872924033328b2c9c8b
MSean <tech@metaline.games>
2023-02-13 22:53:12 +0800

    change mysterybox1155 symbol

--- contracts/mysterybox/MysteryBox1155.sol
+++ contracts/mysterybox/MysteryBox1155.sol
@@ -8,7 +8,7 @@ import "../core/Extendable1155.sol";
 // 1155 id : combine with randomType(uint32) << 32 | mysteryType(uint32)
 contract MysteryBox1155 is Extendable1155 {
 
-    constructor(string memory uri) Extendable1155("Mateline Hero MysteryBox Semi-fungible Token", "MHMB", uri) {
+    constructor(string memory uri) Extendable1155("Mateline MysteryBox Semi-fungible Token", "MLMB", uri) {
         mint(_msgSender(), 0, 1, new bytes(0)); // mint first token to notify event scan
     }
 }
