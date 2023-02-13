084f61544278f033495e51c2548368f0c0a12edd
MSean <tech@metaline.games>
2023-02-13 21:23:20 +0800

    add group 2 test

--- init/Init_Reg.ts
+++ init/Init_Reg.ts
@@ -5,6 +5,6 @@ import { Init_MysteryBoxShop } from "./init_mysteryboxShop";
 
 export function RegAll()
 {  
-    InitTool.RegForGroup("mysteryboxShop", Init_MysteryBoxShop.InitAll, undefined, Init_MysteryBoxShop.ConfigAll,[""]);
+    InitTool.RegForGroup("mysteryboxShop", Init_MysteryBoxShop.InitAll, undefined, Init_MysteryBoxShop.ConfigAll,["test_unusual/test_mysteryboxShop.ts"]);
     InitTool.RegForGroup("mysterybox", Init_MysteryBox.InitAll, undefined, Init_MysteryBox.ConfigAll,["test_unusual/test_mysterybox.ts"]);
 }
--- test_unusual/test_mysteryboxShop.ts
+++ test_unusual/test_mysteryboxShop.ts
@@ -8,8 +8,8 @@ import { TestTool } from '../utils/util_testtool';
 
 describe("mystery box Test", function () {
     before(inittest);
-    it("should openmb succ", openmb);
-    it("should openmb batch succ", openmb_batch);
+    it("should buy mb succ", buymb);
+    it("should buy mb batch succ", buymb_batch);
 });
 
 var testtool: TestTool;
@@ -31,84 +31,9 @@ async function inittest() {
     MockERC20 = await ContractTool.GetVistualContract(testtool.signer0, "MockERC20", "addr:MockERC20");
     MockERC721_V1 = ContractInfo.getContract("MockERC721_V1");
     MockERC1155_V1 = ContractInfo.getContract("MockERC1155_V1");
-    HeroNFT = ContractInfo.getContract("HeroNFT");
-    HeroNFTCodec_V1 = ContractInfo.getContract("HeroNFTCodec_V1");
     MysteryBox1155 = ContractInfo.getContract("MysteryBox1155");
-    HeroNFTMysteryBox = ContractInfo.getContract("HeroNFTMysteryBox");
-    Random = ContractInfo.getContract("Random");
     MysteryBoxShop = ContractInfo.getContract("MysteryBoxShop");
     ContractTool.PassBlock(hre, 1000);
-
-}
-
-async function openmb() {
-
-    let randomid = BigNumber.from(1);
-    let mysterytype = 1;
-
-    //tokenid = (uint64)(randomid)<<32 | (uint32)mysterytype
-    let tokenId = randomid.shl(32).add(1);
-
-    console.log("tokenid=" + tokenId);
-
-
-    await ContractTool.CallState(MysteryBox1155, "mint", [testtool.addr0, tokenId, 1, []]);
-    await ContractTool.CallState(MysteryBox1155, "setApprovalForAll", [HeroNFTMysteryBox.address, true]);
-
-    //open succ
-    logtools.logcyan("item 1 tokenId=" + tokenId);
-
-    //oracleOpenMysteryBox need to take fee
-    var fee = {
-        value: "13000000000000000"
-    }
-    let rc = await ContractTool.CallState(HeroNFTMysteryBox, "oracleOpenMysteryBox", [tokenId, fee]);
-    let ev = ContractTool.GetEvent(rc, "OracleOpenMysteryBox");
-    logtools.loggreen("openmb=" + ev);
-    let reqid = ev[0];
-
-
-    let pinfo = {
-        gasLimit: 5000000,
-        gasPrice: await testtool.provider.getGasPrice(),
-    };
-    await ContractTool.CallState(Random, "fulfillOracleRand", [reqid, 0, pinfo]);
-    logtools.loggreen("test succ");
-}
-async function openmb_batch() {
-
-    let randomid = BigNumber.from(1);
-    let mysterytype = 1;
-
-    //tokenid = (uint64)(randomid)<<32 | (uint32)mysterytype
-    let tokenId = randomid.shl(32).add(1);
-
-    console.log("tokenid=" + tokenId);
-
-
-    await ContractTool.CallState(MysteryBox1155, "mint", [testtool.addr0, tokenId, 10, []]);
-    await ContractTool.CallState(MysteryBox1155, "setApprovalForAll", [HeroNFTMysteryBox.address, true]);
-
-    //open succ
-    logtools.logcyan("item 1 tokenId=" + tokenId);
-
-    //oracleOpenMysteryBox need to take fee
-    var fee = {
-        value: "13000000000000000"
-    }
-    let rc = await ContractTool.CallState(HeroNFTMysteryBox, "batchOracleOpenMysteryBox", [tokenId, 10, fee]);
-    let ev = ContractTool.GetEvent(rc, "BatchOracleOpenMysteryBox");
-    logtools.loggreen("openmb=" + ev);
-    let reqid = ev[0];
-
-
-    let pinfo = {
-        gasLimit: 5000000,
-        gasPrice: await testtool.provider.getGasPrice(),
-    };
-
-    await ContractTool.CallState(Random, "fulfillOracleRand", [reqid, 0, pinfo]);
-    logtools.loggreen("test succ");
 
 }
 
