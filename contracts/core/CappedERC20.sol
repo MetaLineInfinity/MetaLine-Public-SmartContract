7ea3c2217706075ecbe8112c6cd71ed3581fc254
MSean <tech@metaline.games>
2023-03-31 17:58:09 +0800

    remove pausable from cappederc20

--- contracts/core/CappedERC20.sol
+++ contracts/core/CappedERC20.sol
@@ -5,10 +5,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+
 contract CappedERC20 is 
-    Context, 
-    Pausable,
+    Context,
     ERC20Burnable
 {
     constructor(
@@ -18,25 +17,5 @@ contract CappedERC20 is
     ) ERC20(name_, symbol_)
     {
         _mint(_msgSender(), cap_);
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override {
-        super._beforeTokenTransfer(from, to, amount);
-
-        require(!paused(), "ERC20Pausable: token transfer while paused");
-    }
-
-    function pause() external 
-    {
-        _pause();
-    }
-
-    function  unpause() external
-    {
-        _unpause();
     }
 }
