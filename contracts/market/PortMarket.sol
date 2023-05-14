b36390f82f16fcc093b4f5a9517a3d1aa8d82e01
MSean <tech@metaline.games>
2023-05-14 23:19:12 +0800

    add port market

--- contracts/market/PortMarket.sol
+++ contracts/market/PortMarket.sol
@@ -13,9 +13,14 @@ import "./PortMarketPairProxy.sol";
 import "./PortMarketLibrary.sol";
 
 contract PortMarket is ProxyImplInitializer,IPortMarket {
+    using LowGasSafeMath for uint;
     
     event Fee_Changed(uint16 indexed portid, uint16 newFee);
     event Pair_Created(uint16 indexed portid, address indexed token0, uint32 indexed itemid, address pair);
+    
+    event ItemTransfer(uint16 indexed portid, uint32 itemid, address indexed from, address indexed to, uint value);
+    event Off2onChain_item(uint16 indexed portid, uint32 indexed itemid, address indexed addr, uint256 value);
+    event On2offChain_item(uint16 indexed portid, uint32 indexed itemid, address indexed addr, uint256 value);
     
     address public owner;
     address public serviceOp;
@@ -27,7 +32,10 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
     address public swapPairimpl;
     
     // swap pairs
-    mapping(bytes32=>address) swapPair; // keccak256(abi.encodePacked(token0, itemid) => swap pair address
+    mapping(bytes32=>address) public _swapPair; // keccak256(abi.encodePacked(token0, itemid) => swap pair address
+    mapping(address=>uint32) public _swapPairAddrToItemid; // swap pair address => itemid
+
+    mapping(address=>mapping(uint32=>uint256)) _itemBalances; // itemid => user addr => value
     
     // proxy implementation do not use constructor, use initialize instead
     constructor() {}
@@ -87,7 +95,7 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
     }
     function createSwapPair(address token0, uint32 itemid) public override returns(address swapAddr) {
         bytes32 key = _getKey(token0, itemid);
-        require(swapPair[key] == address(0), "PortMarket: pair already created");
+        require(_swapPair[key] == address(0), "PortMarket: pair already created");
 
         swapAddr = address(
             new PortMarketPairProxy{salt: key}(
@@ -95,16 +103,17 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
             )
         );
 
-        swapPair[key] = swapAddr;
+        _swapPair[key] = swapAddr;
+        _swapPairAddrToItemid[swapAddr] = itemid;
 
-        PortMarketPair sp = PortMarketPair(swapAddr);
-        sp.initialize(token0, itemid);
+        PortMarketPair swapPair = PortMarketPair(swapAddr);
+        swapPair.initialize(token0, itemid);
 
         emit Pair_Created(_portID, token0, itemid, swapAddr);
     }
     function getSwapPair(address token0, uint32 itemid) public view override returns(address) {
         bytes32 key = _getKey(token0, itemid);
-        return swapPair[key];
+        return _swapPair[key];
     }
     
     modifier ensure(uint deadline) {
@@ -112,14 +121,14 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
         _;
     }
     function _addLiquidity(
-        address sp,
+        address swapPair,
         uint amount0Desired,
         uint amount1Desired,
         uint amount0Min,
         uint amount1Min,
         address to
     ) internal returns (uint amount0, uint amount1, uint liquidity) {
-        (uint reserve0, uint reserve1,) = IPortMarketPair(sp).getReserves();
+        (uint reserve0, uint reserve1,) = IPortMarketPair(swapPair).getReserves();
         if (reserve0 == 0 && reserve1 == 0) {
             (amount0, amount1) = (amount0Desired, amount1Desired);
         } else {
@@ -135,10 +144,10 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
             }
         }
 
-        address token0 = IPortMarketPair(sp).token0();
-        TransferHelper.safeTransferFrom(token0, msg.sender, sp, amount0);
-        itemTransferFrom(IPortMarketPair(sp).itemid(), msg.sender, sp, amount1);
-        liquidity = IPortMarketPair(sp).mint(to);
+        address token0 = IPortMarketPair(swapPair).token0();
+        TransferHelper.safeTransferFrom(token0, msg.sender, swapPair, amount0);
+        itemTransferFrom(IPortMarketPair(swapPair).itemid(), msg.sender, swapPair, amount1);
+        liquidity = IPortMarketPair(swapPair).mint(to);
     }
 
     function addLiquidity(
@@ -159,7 +168,7 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
     }
 
     function removeLiquidity(
-        address sp, // swap pair address
+        address swapPair, // swap pair address
         uint liquidity, // to remove liquidity lp token value
         uint amount0Min, // amout0 minimum value, if receive amount0 < amount0Min, remove liquidity will be fail
         uint amount1Min, // amout1 minimum value, if receive amount1 < amount1Min, remove liquidity will be fail
@@ -169,8 +178,8 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
         uint amount0, // amount0 of token0 received by user address
         uint amount1 // amount1 of token1 received by user address
     ) {
-        PortMarketPairERC20(sp).transferFrom(msg.sender, sp, liquidity); // send liquidity to pair
-        (amount0, amount1) = IPortMarketPair(sp).burn(to);
+        PortMarketPairERC20(swapPair).transferFrom(msg.sender, swapPair, liquidity); // send liquidity to pair
+        (amount0, amount1) = IPortMarketPair(swapPair).burn(to);
         require(amount0 >= amount0Min, 'PortMarket: INSUFFICIENT_A_AMOUNT');
         require(amount1 >= amount1Min, 'PortMarket: INSUFFICIENT_B_AMOUNT');
     }
@@ -178,48 +187,48 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
     function swapExactTokenForToken(
         uint amountIn, // amount that user want transfer to swap pair contract
         uint amountOutMin, // minimum amount that user want receive, if amountOut < amountOutMin, swap fail
-        address sp, // swap pair contract address
+        address swapPair, // swap pair contract address
         address to, // receive token user address
         uint deadline, // time stamp, in sec, if block.timestamp > deadline, add liquidity will be fail
         bool buy // = ture: token1 swap to token0, = false: token0 swap to token1
     ) external override ensure(deadline) returns (
         uint amountOut // amount of token received by user address
     ) {
-        (uint reserve0, uint reserve1,) = IPortMarketPair(sp).getReserves();
+        (uint reserve0, uint reserve1,) = IPortMarketPair(swapPair).getReserves();
         (uint reserveIn, uint reserveOut) = buy ? (reserve1, reserve0) : (reserve0, reserve1);
         amountOut = PortMarketLibrary.getAmountOut(amountIn, reserveIn, reserveOut, _fee);
         require(amountOut >= amountOutMin, 'PortMarket: INSUFFICIENT_OUTPUT_AMOUNT');
         if(buy) {
-            itemTransferFrom(IPortMarketPair(sp).itemid(), msg.sender, sp, amountIn);
-            IPortMarketPair(sp).swap(amountOut, 0, to, "");
+            itemTransferFrom(IPortMarketPair(swapPair).itemid(), msg.sender, swapPair, amountIn);
+            IPortMarketPair(swapPair).swap(amountOut, 0, to, "");
         }
         else {
-            TransferHelper.safeTransferFrom(IPortMarketPair(sp).token0(), msg.sender, sp, amountIn);
-            IPortMarketPair(sp).swap(0, amountOut, to, "");
+            TransferHelper.safeTransferFrom(IPortMarketPair(swapPair).token0(), msg.sender, swapPair, amountIn);
+            IPortMarketPair(swapPair).swap(0, amountOut, to, "");
         }
     }
 
     function swapTokenForExactToken(
         uint amountOut, // amount that usr want receive
         uint amountInMax, // maximum amount that user allow transfer to swap pair contract, if amountIn > amountInMax, swap fail
-        address sp, // swap pair contract address
+        address swapPair, // swap pair contract address
         address to, // receive token user address
         uint deadline, // time stamp, in sec, if block.timestamp > deadline, add liquidity will be fail
         bool buy // = ture: token1 swap to token0, = false: token0 swap to token1
     ) external override ensure(deadline) returns (
         uint amountIn // amount of token received by swap pair contract
     ) {
-        (uint reserve0, uint reserve1,) = IPortMarketPair(sp).getReserves();
+        (uint reserve0, uint reserve1,) = IPortMarketPair(swapPair).getReserves();
         (uint reserveIn, uint reserveOut) = buy ? (reserve1, reserve0) : (reserve0, reserve1);
         amountIn = PortMarketLibrary.getAmountIn(amountOut, reserveIn, reserveOut, _fee);
         require(amountIn <= amountInMax, 'PortMarket: EXCESSIVE_INPUT_AMOUNT');
         if(buy) {
-            itemTransferFrom(IPortMarketPair(sp).itemid(), msg.sender, sp, amountIn);
-            IPortMarketPair(sp).swap(amountOut, 0, to, "");
+            itemTransferFrom(IPortMarketPair(swapPair).itemid(), msg.sender, swapPair, amountIn);
+            IPortMarketPair(swapPair).swap(amountOut, 0, to, "");
         }
         else {
-            TransferHelper.safeTransferFrom(IPortMarketPair(sp).token0(), msg.sender, sp, amountIn);
-            IPortMarketPair(sp).swap(0, amountOut, to, "");
+            TransferHelper.safeTransferFrom(IPortMarketPair(swapPair).token0(), msg.sender, swapPair, amountIn);
+            IPortMarketPair(swapPair).swap(0, amountOut, to, "");
         }
     }
 
@@ -244,20 +253,36 @@ contract PortMarket is ProxyImplInitializer,IPortMarket {
     }
 
     function off2onChain_item(uint32 itemid, address addr, uint256 value) external override {
+        require(msg.sender == serviceOp, "PortMarket: FORBIDDEN");
 
+        _itemBalances[addr][itemid] = _itemBalances[addr][itemid].add(value);
+        
+        emit Off2onChain_item(_portID, itemid, addr, value);
     }
     function on2offChain_item(uint32 itemid, uint256 value) external override {
+        require(itemBlanceOf(itemid, msg.sender) >= value, "PortMarket: insufficient items");
 
+        _itemBalances[msg.sender][itemid] = _itemBalances[msg.sender][itemid].sub(value);
+
+        emit On2offChain_item(_portID, itemid, msg.sender, value);
     }
 
-    function itemBlanceOf(uint32 itemid, address addr) public override returns(uint256) {
-
+    function itemBlanceOf(uint32 itemid, address addr) public override view returns(uint256) {
+        return _itemBalances[addr][itemid];
+    }
+    
+    function _transfer(uint32 itemid, address from, address to, uint value) private {
+        _itemBalances[from][itemid] = _itemBalances[from][itemid].sub(value);
+        _itemBalances[to][itemid] = _itemBalances[to][itemid].add(value);
+        emit ItemTransfer(_portID, itemid, from, to, value);
     }
     function itemTransfer(uint32 itemid, address to, uint256 value) public override {
-
+        require(msg.sender == address(this) || _swapPairAddrToItemid[msg.sender] != 0, "PortMarket: FORBIDDEN");
+        _transfer(itemid, msg.sender, to, value);
     }
     function itemTransferFrom(uint32 itemid, address from, address to, uint256 value) public override {
-
+        require(msg.sender == address(this) || _swapPairAddrToItemid[msg.sender] != 0, "PortMarket: FORBIDDEN");
+        _transfer(itemid, from, to, value);
     }
 }
 
