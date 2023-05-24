24ffe8e6161679f6d890766fb5d492a66415d584
MSean <tech@metaline.games>
2023-05-24 21:02:37 +0800

    dev commit

--- init/init_assetminter.ts
+++ init/init_assetminter.ts
@@ -38,8 +38,15 @@ export class Init_AssetMinter {
         //function setPackage(uint32 packageId, uint32 totalCount, HeroNFTDataBase[] memory heros, ShipNFTData[] memory ships, uint16[] memory portIDs)
 
         // for Debug ...
-        await ContractTool.CallState(AssetMinter, "setPackage", [1,10,[[0,1,1,1]], [[1,1,1,51,0,1,1]], [1]]); 
-        await ContractTool.CallState(AssetMinter, "setPackage", [2,10,[[0,1,1,1]], [[1,2,3,3,0,1,1]], [1]]); 
+        // await ContractTool.CallState(AssetMinter, "setPackage", [1,10,[[0,1,1,1]], [[1,1,1,51,0,1,1]], [1]]); 
+        // await ContractTool.CallState(AssetMinter, "setPackage", [2,10,[[0,1,1,1]], [[1,2,3,3,0,1,1]], [1]]); 
+
+        await ContractTool.CallState(AssetMinter, "setPackage", [1,100, [[0,1,"281474976841995",256]], [[1,1,1,51,0,1,1]], [1]]);
+        await ContractTool.CallState(AssetMinter, "setPackage", [2,100, [[0,1,"281474976841996",256]], [[1,1,1,51,0,1,1]], [1]]);
+        await ContractTool.CallState(AssetMinter, "setPackage", [3,100, [[0,1,"281474976841997",256]], [[1,1,1,51,0,1,1]], [1]]);
+        await ContractTool.CallState(AssetMinter, "setPackage", [4,100, [[0,1,"281474976841998",256]], [[1,1,1,51,0,1,1]], [1]]);
+        await ContractTool.CallState(AssetMinter, "setPackage", [5,100, [[0,1,"281474976841999",256]], [[1,1,1,51,0,1,1]], [1]]);
+
 
         return true;
     }
