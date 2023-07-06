b46597a7f1147d125fd121f2a9d0d41dfe918ff2
MSean <tech@metaline.games>
2023-07-07 02:20:16 +0800

    dev commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -45,7 +45,12 @@ const config: HardhatUserConfig = {
   },
   zksolc: {
     version: "latest", // Uses latest available in https://github.com/matter-labs/zksolc-bin/
-    settings: {},
+    settings: {
+      optimizer :{
+        enabled: true,
+        mode: "s"
+      }
+    },
   },
   contractSizer:
   {
